cd22032b65f2b788c4dbdbb66afc1bda3b6b96f7
Juan Estrella <juan.estrella137@gmail.com>
2020-09-17 15:00:33 +0200

    Add tab navigation logic

--- javasript/main.js
+++ javasript/main.js
@@ -1,3 +1,5 @@
+// Light/Dark theme
+
 const themeSwitcher = document.getElementById('theme-switch');
 // Check to see if user has set a site preference for dark mode OR
 //  OS - level preference for dark mode AND no site preference
@@ -24,3 +26,70 @@ const toggleTheme = (event) => {
 };
 
 themeSwitcher.addEventListener('click', toggleTheme);
+
+// Set Height of tabs to be the same as the navbar
+
+const navWrapper = document.getElementsByClassName('nav-wrapper');
+const navWrapperHeight = navWrapper[0].clientHeight;
+
+const navigation = document.getElementById('navigation');
+
+const tabs = document.getElementsByClassName('tab');
+
+for (let tab of tabs) {
+  tab.style.height = `${navWrapperHeight - 12}px`;
+}
+
+navigation.style.height = `${navWrapperHeight}px`;
+
+// Tab switching
+
+const summaryTab = document.getElementById('summary-tab');
+const summaryLeft = document.getElementById('summary-left');
+const summaryRight = document.getElementById('summary-right');
+
+const aboutTab = document.getElementById('about-tab');
+const aboutLeft = document.getElementById('about-left');
+const aboutRight = document.getElementById('about-right');
+
+const portfolioTab = document.getElementById('portfolio-tab');
+
+const contactTab = document.getElementById('contact-tab');
+
+const toggleTab = (event) => {
+  for (let tab of tabs) {
+    tab.classList.add('inactive-tab');
+    tab.classList.remove('active', 'active-tab');
+    event.target.classList.remove('inactive-tab');
+    event.path[0].classList.add('active', 'active-tab');
+  }
+};
+
+const showSummary = (event) => {
+  console.log(event.target);
+  if (event.target.classList.contains('active')) {
+    summaryLeft.style.display = '';
+    summaryRight.style.display = '';
+    aboutLeft.style.display = 'none';
+    aboutRight.style.display = 'none';
+  }
+};
+
+const showAbout = (event) => {
+  console.log(event.target);
+  if (event.target.classList.contains('active')) {
+    summaryLeft.style.display = 'none';
+    summaryRight.style.display = 'none';
+    aboutLeft.style.display = '';
+    aboutRight.style.display = '';
+  }
+};
+
+summaryTab.addEventListener('click', toggleTab);
+summaryTab.addEventListener('click', showSummary);
+
+aboutTab.addEventListener('click', toggleTab);
+aboutTab.addEventListener('click', showAbout);
+
+portfolioTab.addEventListener('click', toggleTab);
+contactTab.addEventListener('click', toggleTab);
